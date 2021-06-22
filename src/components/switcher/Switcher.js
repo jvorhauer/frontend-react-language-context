da3529aab4a1b945696f4063b73c8e21ff009b52
Jurjen Vorhauer <jurjen.vorhauer@jdriven.com>
2021-06-22 12:23:58 +0200

    fixed eslint warnings

--- src/components/switcher/Switcher.js
+++ src/components/switcher/Switcher.js
@@ -1,29 +1,30 @@
-import React from 'react';
-import {LangContext} from "../../context/LangContextProvider";
-import {ReactComponent as FlagNL} from '../../assets/netherlands.svg';
-import {ReactComponent as FlagES} from '../../assets/spain.svg';
-
+import React from "react";
+import { LangContext } from "../../context/LangContextProvider";
+import { ReactComponent as FlagNL } from "../../assets/netherlands.svg";
+import { ReactComponent as FlagES } from "../../assets/spain.svg";
 
 const Switcher = () => {
-
   const [selectedLanguage, setSelectedLanguage] = React.useState(true);
   const context = React.useContext(LangContext);
 
   React.useEffect(() => {
     if (selectedLanguage) {
-      context.setDutch()
+      context.setDutch();
     } else {
-      context.setSpanish()
+      context.setSpanish();
     }
-  }, [selectedLanguage]);
+  }, [context, selectedLanguage]);
 
   return (
     <>
-      {!selectedLanguage && <FlagNL onClick={() => setSelectedLanguage(true)} /> }
-      {selectedLanguage && <FlagES onClick={() => setSelectedLanguage(false)} /> }
+      {!selectedLanguage && (
+        <FlagNL onClick={() => setSelectedLanguage(true)} />
+      )}
+      {selectedLanguage && (
+        <FlagES onClick={() => setSelectedLanguage(false)} />
+      )}
     </>
   );
-}
-;
+};
 
 export default Switcher;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import content from '../../data/content.json';
-import {LangContext} from '../../context/LangContextProvider';
+import React from "react";
+import { LangContext } from "../../context/LangContextProvider";
 
 function Home() {
-
-  const {getText} = React.useContext(LangContext);
+  const { getText } = React.useContext(LangContext);
 
   return (
     <div className="page-container">
-      <h2>{getText('home.title')}</h2>
-      <p>{getText('home.introText')}</p>
+      <h2>{getText("home.title")}</h2>
+      <p>{getText("home.introText")}</p>
     </div>
   );
 }
